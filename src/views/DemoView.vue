64ff449cea046b96490e80815019c24cb7713f59
lin <lining0852@126.com>
2023-04-23 21:52:27 +0800

    package init

---
+++ src/views/DemoView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p v-if="a === 'a'"> A </p>
+    <p v-else> B </p>
+    <p v-show="a === 'a'"> A Show</p>
+    <p v-show="a === 'b'"> B Show</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const a = ref('a')
+</script>
+<style lang="less" scoped>
+
+</style>
